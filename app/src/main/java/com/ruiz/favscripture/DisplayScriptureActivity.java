42a7d4aecd5a093044bbd4a981c175b27b06b814
cuervo <59718617+cris-rr@users.noreply.github.com>
2020-05-25 10:22:49 -0500

    Assignment stretch 1, communicating between activities

--- app/src/main/java/com/ruiz/favscripture/DisplayScriptureActivity.java
+++ app/src/main/java/com/ruiz/favscripture/DisplayScriptureActivity.java
@@ -3,23 +3,51 @@ package com.ruiz.favscripture;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DisplayScriptureActivity extends AppCompatActivity {
 
+    String book;
+    String chapter;
+    String verse;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final String TAG = "ScriptureActivityCR";
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_scripture);
 
+        //Getting text values from EXTRAS setting the message for logcat
         Intent intent = getIntent();
-        String book = intent.getStringExtra(MainActivity.EXTRA_BOOK);
-        String chapter = intent.getStringExtra(MainActivity.EXTRA_CHAPTER);
-        String verse = intent.getStringExtra(MainActivity.EXTRA_VERSE);
-
-        TextView scriptureText = findViewById(R.id.scriptureText);
+        book = intent.getStringExtra(MainActivity.EXTRA_BOOK);
+        chapter = intent.getStringExtra(MainActivity.EXTRA_CHAPTER);
+        verse = intent.getStringExtra(MainActivity.EXTRA_VERSE);
         String scripture = String.format("%s %s:%s", book, chapter, verse);
+        Log.d(TAG, String.format("Received intent with %s", scripture));
+
+        //Displaying the scripture in view.
+        TextView scriptureText = findViewById(R.id.scriptureText);
         scriptureText.setText(scripture);
+    }
+
+    public void saveScripture(View view) {
+        SharedPreferences sharedPreferences = getSharedPreferences("scriptureData",
+                MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        //get the data to save in shared preferences
+        editor.putString("book", book);
+        editor.putString("chapter", chapter);
+        editor.putString("verse", verse);
+        editor.commit();
+
+        Toast.makeText(this, "Scripture details are saved!", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/ruiz/favscripture/MainActivity.java
+++ app/src/main/java/com/ruiz/favscripture/MainActivity.java
@@ -3,7 +3,9 @@ package com.ruiz.favscripture;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -12,23 +14,55 @@ public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_CHAPTER ="com.ruiz.favscripture.CHAPTER";
     public static final String EXTRA_VERSE ="com.ruiz.favscripture.VERSE";
 
+    public static final String DEFAULT = "N/A";
+
+    EditText bookText;
+    EditText chapterText;
+    EditText verseText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        bookText = (EditText) findViewById(R.id.bookText);
+        chapterText = (EditText) findViewById(R.id.chapterText);
+        verseText = (EditText) findViewById(R.id.verseText);
+   }
 
     public void displayScripture(View view) {
-        Intent intent = new Intent(this, DisplayScriptureActivity.class);
-        EditText bookText = findViewById(R.id.bookText);
-        EditText chapterText = findViewById(R.id.chapterText);
-        EditText verseText = findViewById(R.id.verseText);
+        final String TAG = "MainActivityCR";
+
+        //getting views and getting the text values
         String book = bookText.getText().toString();
         String chapter = chapterText.getText().toString();
         String verse = verseText.getText().toString();
+
+        //creating the Intent and setting the message for logcat
+        Log.d(TAG, String.format("About to create intent with %s %s:%s", book, chapter, verse));
+        Intent intent = new Intent(this, DisplayScriptureActivity.class);
         intent.putExtra(EXTRA_BOOK, book);
         intent.putExtra(EXTRA_CHAPTER, chapter);
         intent.putExtra(EXTRA_VERSE, verse);
         startActivity(intent);
+    }
+
+    public void loadScripture(View view) {
+        SharedPreferences sharedPreferences = getSharedPreferences("scriptureData",
+                MODE_PRIVATE);
+        String bookSaved = sharedPreferences.getString("book", DEFAULT);
+        String chapterSaved = sharedPreferences.getString("chapter", DEFAULT);
+        String verseSaved = sharedPreferences.getString("verse", DEFAULT);
+        if (!bookSaved.equals(DEFAULT)) {
+            bookText.setText(bookSaved);
+        }
+        if (!chapterSaved.equals(DEFAULT)) {
+            chapterText.setText(chapterSaved);
+        }
+        if (!verseSaved.equals(DEFAULT)) {
+            verseText.setText(verseSaved);
+        }
+
+
+
     }
 }
